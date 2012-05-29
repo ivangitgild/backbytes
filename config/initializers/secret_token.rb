c7bc692930e13ac34659225142d28317476f8e79
Leo Martin Villasica <exst_lvillasica@yahoo.com>
2012-05-29 21:24:37 +0800

    setup rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backbytes::Application.config.secret_token = '36b3c57bd674f7b24ac1ce6173c4f9ff25402e05cc90df91b6362ce9d02b5448fd1cb3feb0b5dfd1f58e937ae3d4718fc08d5b25263ba28cfb0f4f1800938c23'
